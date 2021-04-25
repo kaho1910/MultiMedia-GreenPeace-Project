0c548ccb06a71d933f3c64faf9211f96f1411cc9
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-04-25 21:56:07 +0700

    feat: 🎸 add WASD key control

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ var energy = 5,
     energyMax = 5,
     energyRecharge = 2000; // recharge rate in ms
 var sight = 5; // vision range
-var time = 180; // start time in s
-var cop_walk = 1000; // cop walk in ms
+var time = 1800; // start time in s
 /*----------------------------------------------------------------*/
 
 var mx, my, cx, cy;
@@ -35,8 +34,8 @@ function drawTable() {
             col.setAttribute("x", j);
             col.setAttribute("y", i);
             col.setAttribute("onclick", "moveShip(this)")
-            col.setAttribute("status", 'normal');
-            col.setAttribute("status2", 'normal');
+            col.setAttribute("player", 'normal');
+            col.setAttribute("bot", 'normal');
             col.setAttribute("vision", '0');
             row.appendChild(col);
         }
@@ -49,7 +48,7 @@ function spawnCop() {
     var randX = Math.floor(Math.random() * 2) * 29;
     var randY = Math.floor(Math.random() * 2) * 29;
     var node = document.querySelectorAll(`[x="${randX}"]`)[randY]; //selector
-    node.setAttribute("status2", 'cop'); //cop spwan
+    node.setAttribute("bot", 'cop'); //cop spwan
 }
 
 
@@ -58,7 +57,7 @@ function spawnShip() {
     var randX = Math.floor(Math.random() * 10) + 11;
     var randY = Math.floor(Math.random() * 10) + 11;
     var node = document.querySelectorAll(`[x="${randX}"]`)[randY]; //selector
-    node.setAttribute("status", 'ship') //ship declare
+    node.setAttribute("player", 'ship') //ship spawn
     return [randX, randY];
 }
 
@@ -74,11 +73,12 @@ function moveCop() {
     var mx = hist_x,
         my = hist_y;
     var ship = document.querySelectorAll(`[x="${mx}"]`)[my];
-    cop = document.querySelector(`[status2="cop"]`);
+    cop = document.querySelector(`[bot="cop"]`);
     cx = parseInt(cop.getAttribute("x"));
     cy = parseInt(cop.getAttribute("y"));
-    cop.setAttribute("status2", 'cop_walked');
-    if (ship.getAttribute("status2") == "cop_walked") {
+    cop.setAttribute("bot", 'cop_walked');
+    if (ship.getAttribute("bot") == "cop_walked") {
+        //when step on cop_tile
         if (cx < mx && cy < my) {
             cx++, cy++;
         } else if (cx == mx && cy < my) {
@@ -97,6 +97,7 @@ function moveCop() {
             cx++;
         }
     } else {
+        //cop randomly move
         var skip = 0;
         while (1) {
             var randX = Math.floor(Math.random() * 3) - 1;
@@ -108,7 +109,7 @@ function moveCop() {
                 cy += randY;
             }
             var check = document.querySelectorAll(`[x="${cx}"]`)[cy];
-            if (check.getAttribute("status2") != "cop_walked" || skip == 8) {
+            if (check.getAttribute("bot") != "cop_walked" || skip == 8) {
                 break;
             }
             skip++;
@@ -117,7 +118,7 @@ function moveCop() {
         }
     }
     var cop_stat = document.querySelectorAll(`[x="${cx}"]`)[cy];
-    cop_stat.setAttribute("status2", 'cop');
+    cop_stat.setAttribute("bot", 'cop');
 }
 
 
@@ -139,7 +140,33 @@ setInterval(function() {
     if (!(time % 1)) {
         moveCop();
     }
-}, cop_walk);
+}, 1000);
+
+
+document.addEventListener('keydown', keyEvent, false);
+
+function keyEvent(key) {
+    var check = document.querySelector('[player="ship"]');
+    //console.log(check);
+    var cx = check.getAttribute("x");
+    var cy = check.getAttribute("y");
+    if (key.keyCode == 87) {
+        cy--;
+        //move = document.querySelectorAll(`[x="${cx}"]`)[cy - 1];
+    } else if (key.keyCode == 83) {
+        cy++;
+        //move = document.querySelectorAll(`[x="${cx}"]`)[cy++];
+    } else if (key.keyCode == 65) {
+        cx--;
+        //move = document.querySelectorAll(`[x="${cx - 1}"]`)[cy];
+    } else if (key.keyCode == 68) {
+        cx++;
+        //move = document.querySelectorAll(`[x="${cx++}"]`)[cy];
+    }
+    var move = document.querySelectorAll(`[x="${cx}"]`)[cy];
+    //console.log("--->" + move);
+    moveShip(move);
+}
 
 
 function moveShip(e) {
@@ -152,10 +179,10 @@ function moveShip(e) {
         if ((Math.abs(mx - hist_x) == 1 && Math.abs(my - hist_y) == 1) || (Math.abs(mx - hist_x) == 0 && Math.abs(my - hist_y) == 1) || (Math.abs(mx - hist_x) == 1 && Math.abs(my - hist_y) == 0)) {
             var node = document.querySelectorAll(`[x="${hist_x}"]`)[hist_y];
             //node.style.background = "yellow";
-            node.setAttribute("status", 'ship_walked');
+            node.setAttribute("player", 'ship_walked');
 
             node = document.querySelectorAll(`[x="${mx}"]`)[my];
-            node.setAttribute("status", 'ship');
+            node.setAttribute("player", 'ship');
             hist_x = mx, hist_y = my;
 
             //energy fee
