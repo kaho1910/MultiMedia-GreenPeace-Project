42ad9c6d50f0b6b4e7b459c6e00013cf4e59fc07
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-05-21 20:07:21 +0700

    refactor: rename variables

--- js/main/minigame randomizer.js
+++ js/main/minigame randomizer.js
@@ -1,14 +1,14 @@
 //Minigame Randomizer
 
-var num = 1; // number of game
+var num = 1; // number of minigame
 
-export default function game() {
+export default function miniGame() {
+    var body = document.getElementById("body");
+    var iframe = document.createElement("iframe");
     var rand = Math.floor(Math.random() * num);
     if (rand == 0) {
-        var body = document.getElementById("body");
-        var run = document.createElement("iframe");
-        run.setAttribute("src", "../html/minigame1.html");
-        run.setAttribute("id", "minigame1");
-        body.appendChild(run);
+        iframe.setAttribute("src", "../html/minigame1.html");
+        iframe.setAttribute("id", "minigame1");
+        body.appendChild(iframe);
     }
 }
