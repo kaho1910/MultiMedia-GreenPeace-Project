e4b0a87ebe297243863835d257261eb7a4e01efc
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-05-22 15:29:17 +0700

    refactor: restyle

--- js/main/minigame randomizer.js
+++ js/main/minigame randomizer.js
@@ -1,16 +1,12 @@
 //Minigame Randomizer
 
-var num = 2; // number of minigame
+var num = 3; // number of minigame
 
 export default function miniGame() {
     var body = document.getElementById("body");
     var iframe = document.createElement("iframe");
     iframe.setAttribute("id", "minigame");
-    var rand = Math.floor(Math.random() * num);
-    if (rand == 0) {
-        iframe.setAttribute("src", "../html/minigame1.html");
-    } else if (rand == 1) {
-        iframe.setAttribute("src", "../html/minigame2.html");
-    }
+    var rand = Math.floor(Math.random() * num) + 1;
+    iframe.setAttribute("src", `../html/minigame${rand}.html`);
     body.appendChild(iframe);
 }
