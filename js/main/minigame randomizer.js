c2e60b1f16d57938320bbf89b765546f12fef55b
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-05-24 01:05:05 +0700

    feat: remove minigame 4 (logically)
    logically remove minigame 4

--- js/main/minigame randomizer.js
+++ js/main/minigame randomizer.js
@@ -3,7 +3,12 @@ export default function miniGame(minigame_num) {
     var body = document.getElementById("body");
     var iframe = document.createElement("iframe");
     iframe.setAttribute("id", "minigame");
-    var rand = Math.floor(Math.random() * minigame_num) + 1;
+    while (1) {
+        var rand = Math.floor(Math.random() * minigame_num) + 1;
+        if (rand != 4) {
+            break;
+        }
+    }
     iframe.setAttribute("src", `../html/minigame${rand}.html`);
     body.appendChild(iframe);
 }
