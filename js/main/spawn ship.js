321e5543b89b3ba57805ed629a548f4e12e15154
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-05-23 11:13:19 +0700

    fix: vision when spawn

--- js/main/spawn ship.js
+++ js/main/spawn ship.js
@@ -1,8 +1,18 @@
 //Spawn Ship
-export default function spawnShip() {
+export default function spawnShip(table_size, sight) {
     var randX = Math.floor(Math.random() * 10) + 11;
     var randY = Math.floor(Math.random() * 10) + 11;
-    var node = document.querySelectorAll(`[x="${randX}"]`)[randY]; //selector
-    node.setAttribute("player", 'ship') //ship spawn
+    var ship = document.querySelectorAll(`[x="${randX}"]`)[randY]; //selector
+    ship.setAttribute("player", 'ship') //ship spawn
+    for (let i = 0; i < table_size; i++) {
+        for (let j = 0; j < table_size; j++) {
+            var node = document.querySelectorAll(`[x="${j}"]`)[i];
+            if (Math.abs(randX - j) + Math.abs(randY - i) > sight) {
+                node.style.background = "Tan";
+            } else {
+                node.style.background = "";
+            }
+        }
+    }
     return [randX, randY];
 }
