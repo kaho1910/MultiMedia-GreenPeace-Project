3ce4f7688b44e87d4d85129474353007866d7b63
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-05-23 13:07:44 +0700

    feat: minigame 5 initial

---
+++ js/minigame5/app_g5.js
@@ -0,0 +1,45 @@
+import closeGame from "../main/closeMinigame.js";
+import rewardCheck from "../main/rewardMinigame.js";
+
+const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min
+};
+let number1 = random(-25, 25);
+let number2 = random(-25, 25);
+while (number1 == number2) {
+    number2 = random(0, 50);
+}
+console.log(number1);
+console.log(number2);
+document.querySelector('.results1').innerHTML = number1;
+document.querySelector('.results2').innerHTML = number2;
+
+function funcUp() {
+    number2 += 1;
+    if (number2 > 25) {
+        number2 = -25;
+    }
+    document.querySelector('.results2').innerHTML = number2;
+    if (number1 == number2) {
+        finish();
+    }
+}
+
+function funcDown() {
+    number2 -= 1;
+    if (number2 < -25) {
+        number2 = 25;
+    }
+    document.querySelector('.results2').innerHTML = number2;
+    if (number1 == number2) {
+        finish();
+    }
+}
+
+function finish() {
+    document.getElementsByClassName('results1')[0].style.background = "lightgreen";
+    document.getElementsByClassName('results2')[0].style.background = "lightgreen";
+    console.log("END");
+    closeGame();
+    rewardCheck(true);
+}
